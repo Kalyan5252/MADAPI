7eb7cf34df41bc24abac61ba7a1b5a00d497f323
Kalyan5252 <kalyanpendem007@gmail.com>
2024-06-06 19:13:57 +0530

    login fix

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -29,26 +29,26 @@ export async function POST(req) {
   //     { status: 200 }
   //   );
 
-  //   try {
-  //     const body = await req.json();
-  //     const { userName, password } = body;
-  //     console.log(body);
-  //     console.log(userName, password);
-  //     const user = await Users.findOne({ userName });
-  //     if (!user) throw new Error('User Not Found');
-  //     if (user.password !== password) throw new Error('Incorrect Credentials');
-  //     return NextResponse.json({ message: 'Login Successful' }, { status: 200 });
-  //   } catch (error) {
-  //     return NextResponse.json({ message: error.message }, { status: 404 });
-  //   }
-
   try {
-    // const body = await req.json();
-
-    // console.log(body);
-    console.log('got the req');
-    return NextResponse.json({ message: 'Received ' }, { status: 200 });
+    const body = await req.json();
+    const { userName, password } = body;
+    console.log(body);
+    console.log(userName, password);
+    const user = await Users.findOne({ userName });
+    if (!user) throw new Error('User Not Found');
+    if (user.password !== password) throw new Error('Incorrect Credentials');
+    return NextResponse.json({ message: 'Login Successful' }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: error.message }, { status: 404 });
   }
+
+  // try {
+  //   // const body = await req.json();
+
+  //   // console.log(body);
+  //   console.log('got the req');
+  //   return NextResponse.json({ message: 'Received ' }, { status: 200 });
+  // } catch (error) {
+  //   return NextResponse.json({ message: error.message }, { status: 404 });
+  // }
 }
